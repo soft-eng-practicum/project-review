01af45123d9387b24549b9932d455ff7952d974c
Alcoholicorn <mcoker@ggc.edu>
2017-10-02 09:13:29 -0400

    removed ProjectReview and put all relevant files into the project-review in the htdocs

--- htdocs/ProjectReview/includes/connection_string.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$mysqli = new mysqli("localhost", "ggc_user", "ggc", "ggc_project_review");
-	if ($mysqli->connect_error) {
-		header("Location: ../error.php?message=NO FREAKIN MAIN FRAME");
-		exit();
-	}
-?>
--- htdocs/ProjectReview/includes/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once 'security.php';
-	session_start();
-   	unset($_SESSION["username"]);
-   	unset($_SESSION["password"]);
-   
-   	header("Location: ../index.php");
-	exit();
-?>
