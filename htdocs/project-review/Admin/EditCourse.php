f8a5a8a126f8c8ae122ece4b3f548c8fa1b19ac1
Janna Timmer <timmerjanna@gmail.com>
2017-11-05 00:25:42 -0400

    Add edit course to Admin

---
+++ htdocs/project-review/Admin/EditCourse.php
@@ -0,0 +1,134 @@
+<?php
+	include_once '../includes/connection_string.php';
+	include_once '../includes/security.php';
+	
+	ggc_session();
+	
+	$stmt= $mysqli->query("SELECT * FROM course");
+	
+	echo "This is Edit Course File";
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Edit Course
+		</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+	</head>
+	<body>
+		<?php
+		
+		?>
+		<div class="container">
+			<div class="col-md-6">
+			<table class="table table-hover">
+				<tr>
+					<td>
+						<strong>
+							Name
+						</strong>
+					</td>
+					<td>
+						<strong>
+							Section
+						</strong>
+					</td>
+					<td>
+						<strong>
+							Semester
+						</strong>
+					</td>
+					<td>
+						<strong>
+							Course ID
+						</strong>
+					</td>
+					<td>
+						<strong>
+							Delete?
+						</strong>
+					</td>
+				</tr>
+<?php
+				if($stmt->num_rows != 0)
+				{
+					while($rows = $stmt->fetch_assoc())
+					{
+						$courseID = $rows['course_id'];
+						$name = $rows['name'];
+						$section = $rows['section'];
+						$professorID = $rows['professor_id'];
+						$semester = $rows['semester'];
+						
+						
+						echo "
+						
+						<tr>
+							<td>
+								$name
+							</td>
+							<td>
+								$section
+							</td>
+							<td>
+								$semester
+							</td>
+							<td>
+								$courseID
+							</td>
+							<td>
+								<form action='EditCourse.php' method='post'>
+									<input name='delete_id' value='$courseID' hidden='true'>
+									<input name='delete_name' value='$name' hidden='true'>
+									<input name='delete_section' value='$section' hidden='true'>
+									<input name='delete_semester' value='$semester' hidden='true'>
+									<input type='submit' name='submit_delete' value='X' class='btn btn-danger pull-right'>
+								</form>
+							</td>
+						</tr>";
+					}
+				}
+?>
+			</table>
+			</div>
+			<div class="col-md-6">
+				<form action="EditCourseinc.php" method="get">
+					<div class="form-group">
+						<label for="course_id">
+							<strong>
+								Course ID
+							</strong>
+						</label>
+						<input type="text" class="form-control" name="delete_id">
+					</div>
+					<div class="form-group">
+						<label for="name">
+							<strong>
+								Name
+							</strong>
+						</label>
+						<input type="text" class="form-control" name="delete_name">
+					</div>
+					<div class="form-group">
+						<label for="section">
+							<strong>
+								Section
+							</strong>
+						</label>
+						<input type="text" class="form-control" name="delete_section">
+					</div>
+					<div class="form-group">
+						<label for="semester">
+							<strong>
+								Semester
+							</strong>
+						</label>
+						<input type="text" class="form-control" name="delete_semester">
+					</div>
+					<input type="submit" name="submit_delete" value="Delete" class="btn btn-primary">
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ htdocs/project-review/Admin/EditCourseinc.php
@@ -0,0 +1,30 @@
+<?php
+	include_once '../includes/connection_string.php';
+	include_once '../includes/security.php';
+	ggc_session();
+	
+	if (isset($_GET['delete_id'])
+	{
+		$courseID2=preg_replace("/[^0-9]+/", "", $_GET['delete_id']);
+		
+		$name2=$_GET['delete_name'];
+		$section2=$_GET['delete_section'];
+		$semester2=$_GET['delete_semester'];
+		
+		$delete_stmt= $mysqli->query("DELETE FROM course WHERE course_id = '$courseID2' AND name = '$name2' AND section = '$section2' AND semester = '$semester2'");
+		if ($mysqli->query($delete_stmt) == TRUE)
+		{
+			echo "the delete worked";
+		}
+		else
+		{
+			echo "oh shit, the delete failed";
+		}
+		
+		$delete_stmt->execute();
+		
+		header ('Location: ./EditCourse.php');
+		exit();
+		
+	}
+?>
